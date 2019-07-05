98d89d92010162570d5f5c1fd150220766533aa1
Chris <castachris@hotmail.it>
2019-07-05 21:47:59 +0200

    Test CI_2

--- app/src/routes/routes.js
+++ app/src/routes/routes.js
@@ -3,7 +3,7 @@ module.exports = function(app) {
 
 	app.route('/')
 		.get(function(req, res) {
-			res.send("Test Jenkins CI");
+			res.send("Test Jenkins CI_2");
 		})
 		.post(controller.align_sequences);
 
