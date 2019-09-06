53f9a75f9b193c65fcb366659a5b52e1157b68c6
maicol.forti <maicol.forti@studio.unibo.it>
2019-09-06 18:45:50 +0200

    Fix to margin on s5 hextris

--- app/index.js
+++ app/index.js
@@ -44,7 +44,7 @@ pausecomp(10000);
 console.log('Ten seconds later, ...'); //connessione al db mongoose.set('useFindAndModify', false);
 mongoose.set('connectTimeoutMS', 30); mongoose .connect(
 	'mongodb://mongodb:27017/dbcoffee',
-  //'mongodb://localhost:27017/dbcoffee',
+	//'mongodb://localhost:27017/dbcoffee',
 	//'mongodb://localhost/dbcoffee',
 	// 'mongodb://asw_mongodb_1.asw_interna:27017/dbsa', ANDAVA BENE
 	{ useNewUrlParser: true }) .then(() => console.log('MongoDB Connected')) .catch((err) => console.log(err));
